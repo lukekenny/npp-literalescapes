0a79357de9aa11c178ac3779049b043cfce5efec
lukekenny <lckenny@outlook.com>
2026-01-16 08:22:11 +1100

    Register Decode Escapes plugin menu command

--- src/PluginDefinition.cpp
+++ src/PluginDefinition.cpp
@@ -1,116 +1,103 @@
 //this file is part of notepad++
-//Copyright (C)2022 Don HO <don.h@free.fr>
+//Copyright (C)2021 Don HO <donho@altern.org>
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
 //
 //This program is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //GNU General Public License for more details.
 //
 //You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "PluginDefinition.h"
-#include "menuCmdID.h"
 
-//
-// The plugin data that Notepad++ needs
-//
+// The plugin template expects these globals
+NppData nppData;
 FuncItem funcItem[nbFunc];
 
-//
-// The data of Notepad++ that you can use in your plugin commands
-//
-NppData nppData;
+HINSTANCE g_hInstance = nullptr;
 
-//
-// Initialize your plugin data here
-// It will be called while plugin loading   
+extern void decodeEscapesCommand();
+
+// plugin name
+const TCHAR NPP_PLUGIN_NAME[] = TEXT("LiteralEscapes");
+
+// The data of Notepad++ that you can use in your plugin command
+// in case of need.
+void setInfo(NppData notepadPlusData)
+{
+	nppData = notepadPlusData;
+}
+
+const TCHAR* getName()
+{
+	return NPP_PLUGIN_NAME;
+}
+
+FuncItem* getFuncsArray(int* nbF)
+{
+	*nbF = nbFunc;
+	return funcItem;
+}
+
+void beNotified(SCNotification* /*notifyCode*/)
+{
+	// no-op
+}
+
+LRESULT messageProc(UINT /*Message*/, WPARAM /*wParam*/, LPARAM /*lParam*/)
+{
+	return TRUE;
+}
+
+#ifdef UNICODE
+BOOL isUnicode()
+{
+	return TRUE;
+}
+#endif //UNICODE
+
 void pluginInit(HANDLE /*hModule*/)
 {
+	// no-op
 }
 
-//
-// Here you can do the clean up, save the parameters (if any) for the next session
-//
 void pluginCleanUp()
 {
+	// no-op
 }
 
-//
-// Initialization of your plugin commands
-// You should fill your plugins commands here
 void commandMenuInit()
 {
-
-    //--------------------------------------------//
-    //-- STEP 3. CUSTOMIZE YOUR PLUGIN COMMANDS --//
-    //--------------------------------------------//
-    // with function :
-    // setCommand(int index,                      // zero based number to indicate the order of command
-    //            TCHAR *commandName,             // the command name that you want to see in plugin menu
-    //            PFUNCPLUGINCMD functionPointer, // the symbol of function (function pointer) associated with this command. The body should be defined below. See Step 4.
-    //            ShortcutKey *shortcut,          // optional. Define a shortcut to trigger this command
-    //            bool check0nInit                // optional. Make this menu item be checked visually
-    //            );
-    setCommand(0, TEXT("Hello Notepad++"), hello, NULL, false);
-    setCommand(1, TEXT("Hello (with dialog)"), helloDlg, NULL, false);
+	setCommand(0, TEXT("Decode literal escapes (\\n or \\\\n -> LF)"), decodeEscapesCommand, nullptr, false);
 }
 
-//
-// Here you can do the clean up (especially for the shortcut)
-//
 void commandMenuCleanUp()
 {
-	// Don't forget to deallocate your shortcut here
+	// no-op
 }
 
-
-//
-// This function help you to initialize your plugin commands
-//
-bool setCommand(size_t index, TCHAR *cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey *sk, bool check0nInit) 
+BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD reason, LPVOID /*lpReserved*/)
 {
-    if (index >= nbFunc)
-        return false;
+	switch (reason)
+	{
+		case DLL_PROCESS_ATTACH:
+			g_hInstance = hInst;
+			pluginInit(hInst);
+			break;
 
-    if (!pFunc)
-        return false;
+		case DLL_PROCESS_DETACH:
+			pluginCleanUp();
+			break;
 
-    lstrcpy(funcItem[index]._itemName, cmdName);
-    funcItem[index]._pFunc = pFunc;
-    funcItem[index]._init2Check = check0nInit;
-    funcItem[index]._pShKey = sk;
-
-    return true;
-}
-
-//----------------------------------------------//
-//-- STEP 4. DEFINE YOUR ASSOCIATED FUNCTIONS --//
-//----------------------------------------------//
-void hello()
-{
-    // Open a new document
-    ::SendMessage(nppData._nppHandle, NPPM_MENUCOMMAND, 0, IDM_FILE_NEW);
-
-    // Get the current scintilla
-    int which = -1;
-    ::SendMessage(nppData._nppHandle, NPPM_GETCURRENTSCINTILLA, 0, (LPARAM)&which);
-    if (which == -1)
-        return;
-    HWND curScintilla = (which == 0)?nppData._scintillaMainHandle:nppData._scintillaSecondHandle;
-
-    // Say hello now :
-    // Scintilla control has no Unicode mode, so we use (char *) here
-    ::SendMessage(curScintilla, SCI_SETTEXT, 0, (LPARAM)"Hello, Notepad++!");
-}
-
-void helloDlg()
-{
-    ::MessageBox(NULL, TEXT("Hello, Notepad++!"), TEXT("Notepad++ Plugin Template"), MB_OK);
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+			break;
+	}
+	return TRUE;
 }
