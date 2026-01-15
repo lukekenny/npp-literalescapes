b7b7d470d42f0eded88aa831ef4e65ba8fb85028
lukekenny <lckenny@outlook.com>
2026-01-16 08:22:08 +1100

    Implement escape decoding command (\n/\\n -> LF, \t/\\t -> TAB, quotes, backslashes)

--- src/NppPluginDemo.cpp
+++ src/NppPluginDemo.cpp
@@ -1,105 +1,124 @@
 //this file is part of notepad++
-//Copyright (C)2022 Don HO <don.h@free.fr>
+//Copyright (C)2021 Don HO <donho@altern.org>
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
 //
 //This program is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //GNU General Public License for more details.
 //
 //You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// This plugin was modified for "npp-literalescapes":
+// It adds a Plugins menu command that decodes common literal escape
+// sequences in the current selection (or whole document).
 
 #include "PluginDefinition.h"
 
-extern FuncItem funcItem[nbFunc];
+#include <string>
+#include <vector>
+#include <cstring>
+
+// The plugin template provides these globals in PluginDefinition.cpp
 extern NppData nppData;
 
-
-BOOL APIENTRY DllMain(HANDLE hModule, DWORD  reasonForCall, LPVOID /*lpReserved*/)
+namespace
 {
-	try {
+	HWND getCurrentScintilla()
+	{
+		int which = 0;
+		::SendMessage(nppData._nppHandle, NPPM_GETCURRENTSCINTILLA, 0, reinterpret_cast<LPARAM>(&which));
+		return (which == 0) ? nppData._scintillaMainHandle : nppData._scintillaSecondHandle;
+	}
 
-		switch (reasonForCall)
+	static void replaceAll(std::string& s, const std::string& from, const std::string& to)
+	{
+		if (from.empty()) return;
+		size_t pos = 0;
+		while ((pos = s.find(from, pos)) != std::string::npos)
 		{
-			case DLL_PROCESS_ATTACH:
-				pluginInit(hModule);
-				break;
-
-			case DLL_PROCESS_DETACH:
-				pluginCleanUp();
-				break;
-
-			case DLL_THREAD_ATTACH:
-				break;
-
-			case DLL_THREAD_DETACH:
-				break;
+			s.replace(pos, from.size(), to);
+			pos += to.size();
 		}
 	}
-	catch (...) { return FALSE; }
 
-    return TRUE;
-}
-
-
-extern "C" __declspec(dllexport) void setInfo(NppData notpadPlusData)
-{
-	nppData = notpadPlusData;
-	commandMenuInit();
-}
-
-extern "C" __declspec(dllexport) const TCHAR * getName()
-{
-	return NPP_PLUGIN_NAME;
-}
-
-extern "C" __declspec(dllexport) FuncItem * getFuncsArray(int *nbF)
-{
-	*nbF = nbFunc;
-	return funcItem;
-}
-
-
-extern "C" __declspec(dllexport) void beNotified(SCNotification *notifyCode)
-{
-	switch (notifyCode->nmhdr.code) 
+	// One-step decode: handles either single-backslash or double-backslash forms.
+	// Examples in the *document text*:
+	//   "\\n" OR "\n" -> LF
+	//   "\\t" OR "\t" -> TAB
+	//   "\\\"" OR "\"" -> '"'
+	//   "\\\\" OR "\\" -> '\\'
+	static std::string decodeLiteralEscapes(std::string s)
 	{
-		case NPPN_SHUTDOWN:
-		{
-			commandMenuCleanUp();
-		}
-		break;
+		// Order matters. Decode the longer (double-backslash) patterns first,
+		// then the single-backslash ones.
+		//
+		// We intentionally do NOT produce CRLF; \n becomes LF (Linux style), as requested.
 
-		default:
-			return;
+		replaceAll(s, "\\\\n", "\n");
+		replaceAll(s, "\\\\t", "\t");
+		replaceAll(s, "\\\\\"", "\"");
+		replaceAll(s, "\\\\\\\\", "\\");
+
+		replaceAll(s, "\\n", "\n");
+		replaceAll(s, "\\t", "\t");
+		replaceAll(s, "\\\"", "\"");
+		replaceAll(s, "\\\\", "\\");
+
+		return s;
+	}
+
+	static std::string getTextRange(HWND hSci, Sci_Position start, Sci_Position end)
+	{
+		Sci_TextRangeFull tr{};
+		tr.chrg.cpMin = start;
+		tr.chrg.cpMax = end;
+		std::string buf;
+		buf.resize(static_cast<size_t>(end - start) + 1); // +1 for NUL
+		tr.lpstrText = buf.data();
+		::SendMessage(hSci, SCI_GETTEXTRANGEFULL, 0, reinterpret_cast<LPARAM>(&tr));
+		buf.resize(std::strlen(buf.c_str()));
+		return buf;
+	}
+
+	static void replaceRange(HWND hSci, Sci_Position start, Sci_Position end, const std::string& replacement)
+	{
+		::SendMessage(hSci, SCI_BEGINUNDOACTION, 0, 0);
+		::SendMessage(hSci, SCI_SETTARGETRANGE, start, end);
+		::SendMessage(hSci, SCI_REPLACETARGET, static_cast<WPARAM>(replacement.size()), reinterpret_cast<LPARAM>(replacement.c_str()));
+		::SendMessage(hSci, SCI_ENDUNDOACTION, 0, 0);
 	}
 }
 
-
-// Here you can process the Npp Messages 
-// I will make the messages accessible little by little, according to the need of plugin development.
-// Please let me know if you need to access to some messages :
-// https://github.com/notepad-plus-plus/notepad-plus-plus/issues
-//
-extern "C" __declspec(dllexport) LRESULT messageProc(UINT /*Message*/, WPARAM /*wParam*/, LPARAM /*lParam*/)
-{/*
-	if (Message == WM_MOVE)
-	{
-		::MessageBox(NULL, "move", "", MB_OK);
-	}
-*/
-	return TRUE;
-}
-
-#ifdef UNICODE
-extern "C" __declspec(dllexport) BOOL isUnicode()
+void decodeEscapesCommand()
 {
-    return TRUE;
+	HWND hSci = getCurrentScintilla();
+	if (!hSci) return;
+
+	Sci_Position selStart = static_cast<Sci_Position>(::SendMessage(hSci, SCI_GETSELECTIONSTART, 0, 0));
+	Sci_Position selEnd = static_cast<Sci_Position>(::SendMessage(hSci, SCI_GETSELECTIONEND, 0, 0));
+
+	Sci_Position start = selStart;
+	Sci_Position end = selEnd;
+
+	// If no selection, operate on whole document
+	if (selStart == selEnd)
+	{
+		start = 0;
+		end = static_cast<Sci_Position>(::SendMessage(hSci, SCI_GETLENGTH, 0, 0));
+	}
+
+	if (end <= start) return;
+
+	std::string original = getTextRange(hSci, start, end);
+	std::string decoded = decodeLiteralEscapes(original);
+
+	if (decoded == original) return; // nothing to do
+
+	replaceRange(hSci, start, end, decoded);
 }
-#endif //UNICODE
