464b75ab08422c41f06cf60e370418998396a219
lukekenny <lckenny@outlook.com>
2026-01-16 08:22:12 +1100

    Update plugin command count and declaration

--- src/PluginDefinition.h
+++ src/PluginDefinition.h
@@ -1,76 +1,39 @@
 //this file is part of notepad++
-//Copyright (C)2022 Don HO <don.h@free.fr>
+//Copyright (C)2021 Don HO <donho@altern.org>
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
 //
 //This program is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //GNU General Public License for more details.
 //
 //You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef PLUGINDEFINITION_H
-#define PLUGINDEFINITION_H
+#pragma once
 
-//
-// All difinitions of plugin interface
-//
 #include "PluginInterface.h"
+#include "menuCmdID.h"
+#include "Notepad_plus_msgs.h"
+#include "Scintilla.h"
+#include "Sci_Position.h"
 
-//-------------------------------------//
-//-- STEP 1. DEFINE YOUR PLUGIN NAME --//
-//-------------------------------------//
-// Here define your plugin name
-//
-const TCHAR NPP_PLUGIN_NAME[] = TEXT("Notepad++ plugin template");
+// The number of functions in the function array
+// (update if you add more menu commands)
+constexpr int nbFunc = 1;
 
-//-----------------------------------------------//
-//-- STEP 2. DEFINE YOUR PLUGIN COMMAND NUMBER --//
-//-----------------------------------------------//
-//
-// Here define the number of your plugin commands
-//
-const int nbFunc = 2;
-
-
-//
-// Initialization of your plugin data
-// It will be called while plugin loading
-//
+// Forward decls for the plugin template
 void pluginInit(HANDLE hModule);
-
-//
-// Cleaning of your plugin
-// It will be called while plugin unloading
-//
 void pluginCleanUp();
-
-//
-//Initialization of your plugin commands
-//
 void commandMenuInit();
-
-//
-//Clean up your plugin commands allocation (if any)
-//
 void commandMenuCleanUp();
 
-//
-// Function which sets your command 
-//
-bool setCommand(size_t index, TCHAR *cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey *sk = NULL, bool check0nInit = false);
+// Helper to register commands
+void setCommand(int index, TCHAR* cmdName, PFUNCPLUGINCMD pFunc, ShortcutKey* sk = NULL, bool check0nInit = false);
 
-
-//
-// Your plugin command functions
-//
-void hello();
-void helloDlg();
-
-#endif //PLUGINDEFINITION_H
+// Plugin command
+void decodeEscapesCommand();
